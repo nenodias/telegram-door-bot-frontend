f58b4f337ede7f76a2bc06b0de05be16b3e49b75
nenodias <horacio.dias92@gmail.com>
2021-06-19 19:10:22 -0300

    Fixing radio side-effect after call person

--- src/components/Apartamentos.js
+++ src/components/Apartamentos.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 const URL = `${window.location.protocol}//${window.location.hostname}:8000`;
 
+const APARTAMENTOS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
 function RadioApartamentos(props) {
-  const APARTAMENTOS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (<div>{APARTAMENTOS.map(name => (
     <div className="control" key={name}>
       <label className="radio">
-        <input className="radio" type="radio" name="apartamento" checked={props.value === name} value={name} onChange={e => props.onChange(e.target.value)} />
+        <input className="radio" type="radio" name="apartamento" value={name} onChange={e => props.onChange(e.target.value)} />
         Apartamento {name}
       </label>
     </div>
@@ -17,16 +18,25 @@ function RadioApartamentos(props) {
 function Apartamentos() {
   const [visitante, setVisitante] = useState("");
   const [apartamento, setApartamento] = useState(0);
+  const [state, setState] = useState(true);
+
+  useEffect(() => {
+    if(!state){
+      setState(true);
+    }
+  });
+
   const avisar = () => {
     const callback = (res) => {
       alert(res.data);
       setApartamento(0);
       setVisitante('');
+      setState(false);//Invalidando o state para não renderizar mais a lista. (Para renderizar a mesma atualizada posteriormente pelo useEffect)
     };
-    if(apartamento){
+    if (apartamento) {
       console.log(`Avisando o apartamento ${apartamento} que o ${visitante} chegou!`);
-      
-      if(!visitante){
+
+      if (!visitante) {
         axios.get(`${URL}/door/${apartamento}`).then(callback);
       } else {
         axios.get(`${URL}/door/${apartamento}/${visitante}`).then(callback);
@@ -50,7 +60,7 @@ function Apartamentos() {
           </div>
         </div>
 
-        <RadioApartamentos value={apartamento} onChange={setApartamento} />
+        {(state ? <RadioApartamentos value={apartamento} onChange={setApartamento} /> : <span></span>)}
 
         <button type="button" className="button is-primary" onClick={avisar}>Avisar</button>
       </form>
